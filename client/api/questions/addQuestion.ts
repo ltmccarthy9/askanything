3a44afea3f181f0c61774cca12275631e7b338fe
Liam McCarthy <ltmccarthy9@gmail.com>
2023-03-02 13:44:24 -0600

    deploy

--- client/api/questions/addQuestion.ts
+++ client/api/questions/addQuestion.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '../auth/[...nextauth]'
-import prisma from '../../../prisma/client'
+import prisma from '../../prisma/client'
 
 export default async function handler(
   req: NextApiRequest,
