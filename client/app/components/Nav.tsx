b5d93c99e35c7861d3b414d520cd7401c44a1a9f
Liam McCarthy <ltmccarthy9@gmail.com>
2023-03-04 14:35:58 -0600

    redundant links

--- client/app/components/Nav.tsx
+++ client/app/components/Nav.tsx
@@ -9,16 +9,11 @@ export default async function Nav(){
     console.log(session)
     return(
         <nav className='bg-white p-2 border-b border-gray-300 mb-12'>
-            <div className='flex max-w-4xl justify-between mx-auto'>
+            <div className='flex max-w-7xl justify-between mx-auto'>
                 <Link href={'/'}>
                     <h1 className='font-extrabold text-2xl tracking-wide pt-2'>Ask Anything.</h1>
                 </Link>
                 <ul className='flex items-center gap-6'>
-                    <Link href={'/'}>
-                        <div className='flex p-2 rounded-lg hover:bg-gray-200 h-12'>
-                            <h2 className='my-auto'>Questions</h2>
-                        </div>
-                    </Link>
                     {!session?.user && <Login/>}
                     {session?.user && <Logged image={session.user?.image || ""} />}
                 </ul>
